6b89113c40777ed3693de3e7c8a22cbc201700c2
68freeworld <b00097219@aus.edu>
2025-08-05 00:59:16 +0400

    Fixing Vercel Error Logs32

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_Ce3y0v9c.mjs';
--- .vercel/output/static/_astro/ProgressSteps.CUq93u8d.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"./ProgressSteps.Dr4pHPLH.js";import"./jsx-runtime.EKYJJIwR.js";export{e as default};
