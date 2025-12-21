1850e0c1f848452976b41afbba026263980262ae
68freeworld <b00097219@aus.edu>
2025-12-21 17:08:15 +0400

    Add Vercel adapter for serverless functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://gcintle.com",
+  output: "server",
+  adapter: vercel(),
   integrations: [sitemap()],
 });
