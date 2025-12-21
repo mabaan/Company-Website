3f9655d01db462f123e055e7f121a377754e5e5c
68freeworld <b00097219@aus.edu>
2025-12-21 17:15:15 +0400

    Add React and Tailwind integrations to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://gcintle.com",
   output: "server",
   adapter: vercel(),
-  integrations: [sitemap()],
+  integrations: [react(), tailwind(), sitemap()],
 });
