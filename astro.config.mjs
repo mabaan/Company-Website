b30dfce993c3041c8547764946a556929886e5c6
68freeworld <b00097219@aus.edu>
2025-12-21 17:27:39 +0400

    Fixing vercel issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel"; // <-- use /static
 
 export default defineConfig({
-  site: "https://gcintle.com",
-  output: "hybrid",
-  adapter: vercel(),
-  integrations: [react(), tailwind(), sitemap()],
+  adapter: vercel(), // this will auto-pick /static if you use `output: "static"`
+  integrations: [
+    react(),
+    tailwind(),
+  ],
+  // REMOVE or comment out output: "server"
 });
